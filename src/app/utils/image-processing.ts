894f5bf5051c602908ffc90294e0e8003d7c7458
Gamer <gamertechs.defi@gmail.com>
2025-04-12 01:10:11 +0100

    fix

--- src/app/utils/image-processing.ts
+++ src/app/utils/image-processing.ts
@@ -13,27 +13,27 @@ export async function removeBackground(fileBuffer: Buffer): Promise<Buffer> {
 /**
  * Applies a tint to an image
  */
-export async function applyTint(
-  imageBuffer: Buffer, 
-  tintColor = { r: 102, g: 212, b: 255 }, 
-  tintFactor = 0.2
-): Promise<Buffer> {
-  const image = await Jimp.read(imageBuffer);
+// export async function applyTint(
+//   imageBuffer: Buffer, 
+//   tintColor = { r: 102, g: 212, b: 255 }, 
+//   tintFactor = 0.2
+// ): Promise<Buffer> {
+//   const image = await Jimp.read(imageBuffer);
   
-  image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(x, y, idx) {
-    if (this.bitmap.data[idx + 3] > 0) {
-      let r = this.bitmap.data[idx + 0];
-      let g = this.bitmap.data[idx + 1];
-      let b = this.bitmap.data[idx + 2];
+//   image.scan(0, 0, image.bitmap.width, image.bitmap.height, function(x, y, idx) {
+//     if (this.bitmap.data[idx + 3] > 0) {
+//       let r = this.bitmap.data[idx + 0];
+//       let g = this.bitmap.data[idx + 1];
+//       let b = this.bitmap.data[idx + 2];
 
-      this.bitmap.data[idx + 0] = Math.round(r * (1 - tintFactor) + tintColor.r * tintFactor);
-      this.bitmap.data[idx + 1] = Math.round(g * (1 - tintFactor) + tintColor.g * tintFactor);
-      this.bitmap.data[idx + 2] = Math.round(b * (1 - tintFactor) + tintColor.b * tintFactor);
-    }
-  });
+//       this.bitmap.data[idx + 0] = Math.round(r * (1 - tintFactor) + tintColor.r * tintFactor);
+//       this.bitmap.data[idx + 1] = Math.round(g * (1 - tintFactor) + tintColor.g * tintFactor);
+//       this.bitmap.data[idx + 2] = Math.round(b * (1 - tintFactor) + tintColor.b * tintFactor);
+//     }
+//   });
 
-  return await image.getBufferAsync(Jimp.MIME_PNG);
-}
+//   return await image.getBufferAsync(Jimp.MIME_PNG);
+// }
 
 /**
  * Applies a background to an image
